7c84f0528d044c72e79e8ef815afa8cbdf558a25
Christopher Staudinger <christopherstaudinger@Christophers-MacBook-Pro.local>
2019-04-24 10:44:13 +1000

    added interface styling

--- main.rb
+++ main.rb
@@ -1,16 +1,16 @@
 require 'csv'
 require 'paint'
+require 'tty-font'
+
+
+font = TTY::Font.new(:doom)
+puts font.write("Welcome To")
+puts font.write("Article Checker")
 
 prompt_user_file_path = "Please input the file path of the file you would like to have checked"
-puts Paint[prompt_user_file_path, :gold, :bright]
+puts Paint[prompt_user_file_path, :blue, :bright]
 file_path = gets().chomp()
 input_file = File.read(file_path)
-
-# input_file = File.read('./test.txt')
-# puts input_file
-# input_arr = []
-# print input_arr = input_file.split(' ')
-
 
 #### cleans imported text
 # cleaned_string = input_file.gsub(/[^\p{Alnum} -]/," ").downcase()
@@ -21,18 +21,17 @@ input_arr = cleaned_string.split(" ")
 
 
 res_hash = Hash.new
-# print res_hash
 arrindex = 0
 while (arrindex < input_arr.length)
   k = input_arr.count{|x| x == input_arr[arrindex]}
-  res_hash[input_arr[arrindex]] = k
+  res_hash[input_arr[arrindex].to_sym] = k
   arrindex +=1
 end
 # print res_hash
 # print "\n", res_hash.to_a
 
-prompt_user_name_output_file = "Input a name of your choice for the resulting data to be stored"
-puts Paint[prompt_user_name_output_file, :green, :bright]
+prompt_user_to_name_output_file = "Input a name of your choice for the resulting data to be stored"
+puts Paint[prompt_user_to_name_output_file, :blue, :bright]
 
 output_file_name = gets().chomp()
 
